70cdfbda3215638c15e0a5832985a02f36e644ad
Caroline <cyham@umich.edu>
2019-04-04 14:24:32 -0400

    Fixed formatting

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -1,3 +1,4 @@
+
 /*
  * testSelectionSort.java
  *
@@ -18,114 +19,109 @@ public class testSelectionSort {
 		testDuplicates();
 	}
 
-    public testSelectionSort() {
-    }
-
-    public void testPositive(){
-        int[] arr = new int[5];
-        arr[0] = 8;
-        arr[1] = 9;
-        arr[2] = 7;
-        arr[3] = 10;
-        arr[4] = 2;
-
-        int[] Sortedarr = new int[5];
-        Sortedarr[0] = 2;
-        Sortedarr[1] = 7;
-        Sortedarr[2] = 8;
-        Sortedarr[3] = 9;
-        Sortedarr[4] = 10;
-
-				SelectionSort sortObj = new SelectionSort();
-
-				arr = sortObj.basicSelectionSort(arr);
-
-				// Want to know where it failed
-				for (int index = 0; index < 5; index++)
-				{
-					assertEquals("Array failed at index " + Integer.toString(index), Sortedarr[index], arr[index]);
-				}
-    }
-
-    public void testNegative(){
-			int[] arr = new int[5];
-			arr[0] = -8;
-			arr[1] = -9;
-			arr[2] = -7;
-			arr[3] = -10;
-			arr[4] = -2;
-
-			int[] Sortedarr = new int[5];
-			Sortedarr[0] = -10;
-			Sortedarr[1] = -9;
-			Sortedarr[2] = -8;
-			Sortedarr[3] = -7;
-			Sortedarr[4] = -2;
-
-			SelectionSort sortObj = new SelectionSort();
-
-			arr = sortObj.basicSelectionSort(arr);
-
-			// Want to know where it failed
-			for (int index = 0; index < 5; index++)
-			{
-				assertEquals("Array failed at index " + Integer.toString(index), Sortedarr[index], arr[index]);
-			}
-    }
-
-    public void testMixed(){
-			int[] arr = new int[5];
-      arr[0] = -8;
-      arr[1] = 9;
-      arr[2] = 0;
-      arr[3] = -10;
-      arr[4] = 2;
-
-      int[] Sortedarr = new int[5];
-      Sortedarr[0] = -10;
-      Sortedarr[1] = -8;
-      Sortedarr[2] = 0;
-      Sortedarr[3] = 2;
-      Sortedarr[4] = 9;
-
-			SelectionSort sortObj = new SelectionSort();
-
-			arr = sortObj.basicSelectionSort(arr);
-
-			// Want to know where it failed
-			for (int index = 0; index < 5; index++)
-			{
-				assertEquals("Array failed at index " + Integer.toString(index), Sortedarr[index], arr[index]);
-			}
-    }
-
-    public void testDuplicates(){
-			int[] arr = new int[6];
-			arr[0] = -8;
-			arr[1] = 9;
-			arr[2] = 0;
-			arr[3] = -8;
-			arr[4] = 9;
-			arr[5] = 0;
-
-			int[] Sortedarr = new int[6];
-			Sortedarr[0] = -8;
-			Sortedarr[1] = -8;
-			Sortedarr[2] = 0;
-			Sortedarr[3] = 0;
-			Sortedarr[4] = 9;
-			Sortedarr[5] = 9;
-
-			SelectionSort sortObj = new SelectionSort();
-
-			arr = sortObj.basicSelectionSort(arr);
-
-			// Want to know where it failed
-			for (int index = 0; index < 6; index++)
-			{
-				assertEquals("Array failed at index " + Integer.toString(index), Sortedarr[index], arr[index]);
-			}
+	public testSelectionSort() {
 	}
 
+	public void testPositive() {
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
+
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = 2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = 9;
+		Sortedarr[4] = 10;
+
+		SelectionSort sortObj = new SelectionSort();
+
+		arr = sortObj.basicSelectionSort(arr);
+
+		// Want to know where it failed
+		for (int index = 0; index < 5; index++) {
+			assertEquals("Array failed at index " + Integer.toString(index), Sortedarr[index], arr[index]);
+		}
+	}
+
+	public void testNegative() {
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -9;
+		arr[2] = -7;
+		arr[3] = -10;
+		arr[4] = -2;
+
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -10;
+		Sortedarr[1] = -9;
+		Sortedarr[2] = -8;
+		Sortedarr[3] = -7;
+		Sortedarr[4] = -2;
+
+		SelectionSort sortObj = new SelectionSort();
+
+		arr = sortObj.basicSelectionSort(arr);
+
+		// Want to know where it failed
+		for (int index = 0; index < 5; index++) {
+			assertEquals("Array failed at index " + Integer.toString(index), Sortedarr[index], arr[index]);
+		}
+	}
+
+	public void testMixed() {
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = 9;
+		arr[2] = 0;
+		arr[3] = -10;
+		arr[4] = 2;
+
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -10;
+		Sortedarr[1] = -8;
+		Sortedarr[2] = 0;
+		Sortedarr[3] = 2;
+		Sortedarr[4] = 9;
+
+		SelectionSort sortObj = new SelectionSort();
+
+		arr = sortObj.basicSelectionSort(arr);
+
+		// Want to know where it failed
+		for (int index = 0; index < 5; index++) {
+			assertEquals("Array failed at index " + Integer.toString(index), Sortedarr[index], arr[index]);
+		}
+	}
+
+	public void testDuplicates() {
+		int[] arr = new int[6];
+		arr[0] = -8;
+		arr[1] = 9;
+		arr[2] = 0;
+		arr[3] = -8;
+		arr[4] = 9;
+		arr[5] = 0;
+
+		int[] Sortedarr = new int[6];
+		Sortedarr[0] = -8;
+		Sortedarr[1] = -8;
+		Sortedarr[2] = 0;
+		Sortedarr[3] = 0;
+		Sortedarr[4] = 9;
+		Sortedarr[5] = 9;
+
+		SelectionSort sortObj = new SelectionSort();
+
+		arr = sortObj.basicSelectionSort(arr);
+
+		// Want to know where it failed
+		for (int index = 0; index < 6; index++) {
+			assertEquals("Array failed at index " + Integer.toString(index), Sortedarr[index], arr[index]);
+		}
+	}
 
 }
